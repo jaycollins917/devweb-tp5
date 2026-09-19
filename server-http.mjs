cd5c06e3021f2c01194bf9c0dd9cb17ed072f81e
jaycollins917 <jaycollins917@gmail.com>
2026-09-19 11:08:00 +1100

    feat: ajout de prettier et eslint

--- server-http.mjs
+++ server-http.mjs
@@ -2,8 +2,7 @@ import http from "node:http";
 import fs from "node:fs/promises";
 
 const host = "localhost";
-const port = 8000;            // http://localhost:8000/
-
+const port = 8000; // http://localhost:8000/
 
 /* requestListener n°1
 function requestListener(_request, response) {
@@ -11,7 +10,6 @@ function requestListener(_request, response) {
   response.end(JSON.stringify({ message: "I'm OK" }));
 }
 */
-
 
 /* requestListener n°2
   function requestListener(_request, response) {
@@ -47,22 +45,19 @@ function requestListener(_request, response) {
 // requestListener n°4 en mode async / await
 
 async function requestListener(_request, response) {
-
   try {
     const contents = await fs.readFile("index.html", "utf8");
     response.setHeader("Content-Type", "text/html");
     response.writeHead(200);
     return response.end(contents);
-  } 
-
-  catch (error) {
+  } catch (error) {
     console.error(error);
     response.writeHead(500, { "Content-Type": "text/html" });
-    return response.end("<html><h1>Erreur interne - fichier non trouve</h1></html>");
+    return response.end(
+      "<html><h1>Erreur interne - fichier non trouve</h1></html>",
+    );
   }
 }
-
-
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
@@ -72,3 +67,4 @@ server.listen(port, host, () => {
 console.log("NODE_ENV =", process.env.NODE_ENV);
 console.log("ici");
 console.log("hello");
+
